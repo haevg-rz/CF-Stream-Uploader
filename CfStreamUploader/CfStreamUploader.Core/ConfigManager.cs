004d0050c364f0d94fef2d8ecb515f95cb9111b6
Leo ecker <leo.ecker@haevg-rz.de>
2021-05-04 14:46:22 +0200

    Create ReadConfig and WriteConfig

--- CfStreamUploader/CfStreamUploader.Core/ConfigManager.cs
+++ CfStreamUploader/CfStreamUploader.Core/ConfigManager.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using CfStreamUploader.Core.Models;
+using Newtonsoft.Json;
 
 namespace CfStreamUploader.Core
 {
     public class ConfigManager
     {
+        private static string ConfigPath { get; } =
+            $@"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\CfStreamUploader\Config.JSON";
 
+        private readonly string cfStreamUploaderGuiPath =
+            $@"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\CfStreamUploader";
+
+        public Config ReadConfig()
+        {
+            if (!File.Exists(ConfigPath))
+                return null;
+
+            var jsonString = File.ReadAllText(ConfigPath);
+            var config = JsonConvert.DeserializeObject<Config>(jsonString);
+            return config;
+        }
+
+        public void WriteConfig(Config config)
+        {
+            if (!Directory.Exists(cfStreamUploaderGuiPath))
+                Directory.CreateDirectory(cfStreamUploaderGuiPath);
+
+            var jsonString = JsonConvert.SerializeObject(config, Formatting.Indented);
+            File.WriteAllText(ConfigPath, jsonString);
+
+        }
 
     }
 }
---
+++ CfStreamUploader/CfStreamUploader.Core/Models/Config.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CfStreamUploader.Core.Models
+{
+    public class Config
+    {
+        public string Token { get; set; }
+
+        public Config(string token)
+        {
+            this.Token = token;
+        }
+    }
+}
