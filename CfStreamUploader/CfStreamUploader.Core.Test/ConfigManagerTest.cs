a1bdcb7f98778086efe8d05fdd9336a28b67c154
Leo ecker <leo.ecker@haevg-rz.de>
2021-05-10 14:52:26 +0200

    #4

--- CfStreamUploader/CfStreamUploader.Core.Test/ConfigManagerTest.cs
+++ CfStreamUploader/CfStreamUploader.Core.Test/ConfigManagerTest.cs
@@ -1,93 +1,93 @@
 ﻿using CfStreamUploader.Core.Models;
 using Newtonsoft.Json;
-using NUnit.Framework;
 using System.IO;
+using Xunit;
 
 namespace CfStreamUploader.Core.Test
 {
-    [TestFixture]
     public class ConfigManagerTest
     {
-        [Test]
-        public void TestTest()
+        [Fact(DisplayName = "Mein toller Test mit Tobias Meissner Erarbeitet")]
+        public void TestSuper()
         {
-            Assert.AreEqual("goodTest", "goodTest");
+            Equals(true);
         }
-    //    private ConfigManager ConfigManager { get; } = new ConfigManager();
 
-    //    private readonly string solutionDir =
-    //        Path.GetDirectoryName(Path.GetDirectoryName(TestContext.CurrentContext.TestDirectory));
-
-    //    private const string txtPath = "Config.json";
-
-    //    [TestCase("ReadConfig TestToken", true, "ReadConfig TestToken")]
-    //    [TestCase("ReadConfig TestToken", false, "")]
-    //    public void ReadConfigTest(string token, bool writeFile, string result)
-    //    {
-    //        #region Assign
-
-    //        var config = new Config(token);
-
-    //        if(writeFile)
-    //        {
-    //            var jsonString = JsonConvert.SerializeObject(config, Formatting.Indented);
-    //            File.WriteAllText(Path.Combine(solutionDir,txtPath), jsonString);
-    //        }
-
-    //        #endregion
-
-    //        #region Act
-
-    //        this.ConfigManager.ReadConfig();
-
-    //        #endregion
-
-    //        #region Assert
-
-    //        Assert.AreEqual(result, this.ConfigManager.Config.CfToken);
-    //        #endregion
-    //    }
-
-    //    [Test]
-    //    public void UpdateConfigTest()
-    //    {
-    //        #region Assign
-
-    //        #endregion
-
-    //        #region Act
-
-    //        this.ConfigManager.UpdateConfig("testToken");
-
-    //        #endregion
-
-    //        #region Assert
-
-    //        Assert.That(File.Exists(Path.Combine(this.solutionDir + txtPath)), Is.EqualTo(true));
-
-    //        #endregion
-    //    }
-
-    //    [SetUp]
-    //    public void SetUp()
-    //    {
-    //        this.ConfigManager.ConfigPath = this.solutionDir;
-    //        DeletePath();
-    //    }
-
-    //    [TearDown]
-    //    public void TearDown()
-    //    {
-    //        DeletePath();
-    //    }
-
-    //    public void DeletePath()
-    //    {
-    //        var a = Path.Combine(this.solutionDir, txtPath);
-
-    //        if (File.Exists(Path.Combine(this.solutionDir, txtPath)))
-    //            File.Delete(Path.Combine(this.solutionDir, txtPath));
-
-    //    }
+        // private ConfigManager ConfigManager { get; } = new ConfigManager();
+        //
+        // private readonly string solutionDir =
+        //     Path.GetDirectoryName(Path.GetDirectoryName(TestContext.CurrentContext.TestDirectory));
+        //
+        // private const string txtPath = "Config.json";
+        //
+        // [TestCase("ReadConfig TestToken", true, "ReadConfig TestToken")]
+        // [TestCase("ReadConfig TestToken", false, "")]
+        // public void ReadConfigTest(string token, bool writeFile, string result)
+        // {
+        //     #region Assign
+        //
+        //     var config = new Config(token);
+        //
+        //     if (writeFile)
+        //     {
+        //         var jsonString = JsonConvert.SerializeObject(config, Formatting.Indented);
+        //         File.WriteAllText(Path.Combine(solutionDir, txtPath), jsonString);
+        //     }
+        //
+        //     #endregion
+        //
+        //     #region Act
+        //
+        //     this.ConfigManager.ReadConfig();
+        //
+        //     #endregion
+        //
+        //     #region Assert
+        //
+        //     Assert.AreEqual(result, this.ConfigManager.Config.CfToken);
+        //     #endregion
+        // }
+        //
+        // [Test]
+        // public void UpdateConfigTest()
+        // {
+        //     #region Assign
+        //
+        //     #endregion
+        //
+        //     #region Act
+        //
+        //     this.ConfigManager.UpdateConfig("testToken");
+        //
+        //     #endregion
+        //
+        //     #region Assert
+        //
+        //     Assert.That(File.Exists(Path.Combine(this.solutionDir + txtPath)), Is.EqualTo(true));
+        //
+        //     #endregion
+        // }
+        //
+        // [SetUp]
+        // public void SetUp()
+        // {
+        //     this.ConfigManager.ConfigPath = this.solutionDir;
+        //     DeletePath();
+        // }
+        //
+        // [TearDown]
+        // public void TearDown()
+        // {
+        //     DeletePath();
+        // }
+        //
+        // public void DeletePath()
+        // {
+        //     var a = Path.Combine(this.solutionDir, txtPath);
+        //
+        //     if (File.Exists(Path.Combine(this.solutionDir, txtPath)))
+        //         File.Delete(Path.Combine(this.solutionDir, txtPath));
+        //
+        // }
     }
 }
