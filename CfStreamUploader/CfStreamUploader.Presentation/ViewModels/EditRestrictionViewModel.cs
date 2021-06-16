9bf30641f8db2861c8ee63d639f2e98751e21c19
Leo ecker <leo.ecker@haevg-rz.de>
2021-06-16 14:44:44 +0200

    #43

--- CfStreamUploader/CfStreamUploader.Presentation/ViewModels/EditRestrictionViewModel.cs
+++ CfStreamUploader/CfStreamUploader.Presentation/ViewModels/EditRestrictionViewModel.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Net;
+using System.Text;
 using System.Windows;
 
 namespace CfStreamUploader.Presentation.ViewModels
@@ -170,10 +171,10 @@ namespace CfStreamUploader.Presentation.ViewModels
 
         private bool IsValidId(List<string> ipStrings)
         {
-            return true; //TODO
             foreach (var ipString in ipStrings)
             {
-                var result = IPAddress.TryParse(ipString, out var ipAdress);
+                var ipStringWithoutSubnetMask = ipString.Split("/").ToList();
+                var result = IPAddress.TryParse(ipStringWithoutSubnetMask[0], out var ipAdress);
                 if (!result)
                     return false;
             }
