415a6c437711ae2fa14261dfdd2c21f4be5e6935
Tobias Meissner <tobias.meissner@haevg-rz.de>
2022-10-26 11:08:11 +0200

    cleanup CfLogger

--- CfStreamUploader/CfStreamUploader.Core/CfLogger.cs
+++ CfStreamUploader/CfStreamUploader.Core/CfLogger.cs
@@ -13,10 +13,7 @@ namespace CfStreamUploader.Core
             if (!File.Exists(folderpath))
                 Directory.CreateDirectory(folderpath);
 
-            var time = DateTime.Now;
-            //DateTime.Now.ToString("o") +
             var filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "CfStreamUploader", "Logger", DateTime.Now.ToString("yyyyMMddHHmmssfff") + "log.txt");
-
             var stream = File.CreateText(filePath);
             await stream.WriteLineAsync(DateTime.Now.ToString("o")+ e.Message + e.StackTrace);
             stream.Close();
